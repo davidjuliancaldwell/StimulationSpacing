bea6cf83351db2786179ebb79bedfaa7a64a0a06
Jeneva Cronin <jcronin@cs.washington.edu>
2016-07-07 17:31:55 -0700

    Added section to main script (spectralAnalysisScript) to plot all 10 high/low/med pulses on one graph. Added spectralAnalysisComp to do the actual computation of the fft and then the normalization for the one-sided plot. Updated spectralAnalysis function to call this computation functino. Added .gitignore file to ignore .asv files.

--- spectralAnalysis.m
+++ spectralAnalysis.m
@@ -2,15 +2,8 @@
 
 % make sure data is loaded in 
 function [f,P1] = spectralAnalysis(fs_data,t,dataEpoched)
-fs = fs_data;
-T = 1/fs;
-L = length(dataEpoched);
 
-Y = fft(dataEpoched);
-P2 = abs(Y/L);
-P1 = P2(1:floor(L/2)+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = fs*(0:floor(L/2))/L;
+[f,P1] = spectralAnalysisComp(fs_data,dataEpoched);
 
 subplot(3,1,1)
 plot((f),(P1),'linewidth',[2])
