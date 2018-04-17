5e869868d3e5ac62423375845e1d302be15a773a
David Caldwell <djcald@cs.washington.edu>
2018-04-16 17:23:11 -0700

    moved much of the resistivity extraction work to the electrode modeling project

--- Z_ConstantsStimSpacing.m
+++ Z_ConstantsStimSpacing.m
@@ -1,8 +1,7 @@
-SIDS = {'3f2113','20f8a3','2fd831','2a8d14'};
+SIDS = {'3f2113','20f8a3','2fd831','2a8d14','d62e38'};
 
-OUTPUT_DIR = fullfile(myGetenv('OUTPUT_DIR'), 'stimSpacing', 'figures');
+OUTPUT_DIR = fullfile(myGetenv('OUTPUT_DIR'), 'stimSpacing', 'outputData');
 TouchDir(OUTPUT_DIR);
 META_DIR = fullfile(myGetenv('OUTPUT_DIR'), 'stimSpacing', 'meta','1secBeforeAfter');
 TouchDir(META_DIR);
 
-%OUTPUT_DIR = char(System.IO.Path.GetFullPath(OUTPUT_DIR)); % modified DJC 7-23-2015 - temporary fix to save figures
